6a5c582974002d70ddd2636bdc270dd664da8ac2
Gunjan Gidwani <gunjan.g@axisrooms.com>
2020-01-27 11:08:02 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import  Header  from "./assets/Component/Header";
 import Delivery from "./assets/Component/delivery";
+import Testimonial from "./assets/Component/testimonial";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Delivery />
+      <Testimonial />
     </div>
   );
 }
--- src/assets/Component/delivery.jsx
+++ src/assets/Component/delivery.jsx
@@ -1,51 +1,83 @@
 import React, { Component } from 'react'
-import { Container, Row, Col, Image} from "react-bootstrap";
+import { Container, Row, Col, Image, ListGroup} from "react-bootstrap";
 import content1 from "../Images/content1.png";
 import content2 from "../Images/content2.png";
+import tick from "../Images/tick.png"
+import khata from "../Images/khata.png"
 
 class Delivery extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            
-        }
-    }
-
     render() {
         return (
-            <Container>
-                <Row>
-                   <Col>
-                   <h3>Heading</h3>
-                        list
+            <Container fluid={true}>
+                <Row className="align-items-center justify-content-center m-5 p-2"> 
+                    <h4 className="text-muted"> Why Customer <span className="font-weight-bold"> {" "} loves </span> </h4>
+                </Row>
+                <Row className=" m-1 p-4">
+                    <Col xs={12} md={6} >
+                        <Image style={{width: '100%'}} src={content1} className="img-fluid"/>
+                    </Col> 
+                   <Col xs={12} md={6} className="pl-5">
+                        <div>
+                        <h4 className="headingColor"> <span className="font-weight-bold">Daily</span> Usable from nearby shops</h4>
+                        <div className="pt-3" style={{fontSize: '10px'}}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fugiat, atque deleniti cum eligendi reiciendis doloremque sequi sunt earum enim, nam excepturi magnam delectus dolor illum dolorum esse ab possimus!</div>
+                        <ul className="pt-5 mt-3 pl-0">
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                        </ul>
+                        </div>
                     </Col>
-                    <Col>
-                        <Image src={content1} rounded />
+
+                </Row>
+                <Row className=" m-1 p-4">
+                   <Col xs={12} md={6} className="pl-5">
+                        <div>
+                        <h4 className="headingColor"> <span className="font-weight-bold">Daily</span> Usable from nearby shops</h4>
+                        <div className="pt-3" style={{fontSize: '10px'}}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fugiat, atque deleniti cum eligendi reiciendis doloremque sequi sunt earum enim, nam excepturi magnam delectus dolor illum dolorum esse ab possimus!</div>
+                        <ul className="pt-5 mt-3 pl-0">
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                        </ul>
+                        </div>
+                    </Col>
+                    <Col xs={12} md={6} >
+                        <Image style={{width: '100%'}} src={content2} className="img-fluid"/>
                     </Col> 
                 </Row>
-                <Row>
-                    <Col>
-                        <Image src={content2} rounded />
-                    </Col> 
-                   <Col>
-                   <h3>Heading</h3>
-                        list
+                <Row id="navbarNav" className="pt-4" style={{color: 'white'}}>
+                    <Col sm={12} md={8} xs={12} className=" d-flex">
+                        <div>
+                        <h4  > khata Book</h4>
+                   <div className="" style={{fontSize: '10px'}}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fugiat, atque deleniti cum eligendi reiciendis doloremque sequi sunt earum enim, nam excepturi magnam delectus dolor illum dolorum esse ab possimus!</div>
+                        <ul className="pt-3 mt-3 pl-0">
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                        </ul>
+                        </div>
+                        <Image style={{width: '60%', height: '80%'}} src={khata} />
                     </Col>
-                </Row>
-                <Row>
-                    <Col>
-                    1
-                    </Col>
-                    <Col>
-                    2
-                    </Col>
-                    <Col>
-                    3
+                    <Col sm={12 } md={4} xs={12}>
+                    <h4> Credit Pay</h4>
+                   <div className="" style={{fontSize: '10px'}}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fugiat, atque deleniti cum eligendi reiciendis doloremque sequi sunt earum enim, nam excepturi magnam delectus dolor illum dolorum esse ab possimus!</div>
+                        <ul className="pt-3 mt-3 pl-0">
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                            <li style={{listStyleType: 'none'}}>  <Image style={{height: '15px', width: '13px', marginRight: '7px'}} src={tick} alt=""/>  Eos fugiat, atque deleniti cum e </li>
+                        </ul>
                     </Col>
                 </Row>
                 
-            </Container>
+            </Container >
             
         )
     }
