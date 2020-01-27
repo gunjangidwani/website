15a629069e421bedc26b87bc1949c348a0e434aa
Gunjan Gidwani <gunjan.g@axisrooms.com>
2020-01-27 11:15:47 +0530

    Updates

---
+++ precache-manifest.9603052c4b224740b4fb37ca92be0757.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2693b0a3f3557854d0d6392091cb7dc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3feda6b9d42fa98c8a5",
+    "url": "/static/css/2.49b7fb45.chunk.css"
+  },
+  {
+    "revision": "0b43348f140784b09f76",
+    "url": "/static/css/main.9fb99f6c.chunk.css"
+  },
+  {
+    "revision": "c3feda6b9d42fa98c8a5",
+    "url": "/static/js/2.58ba97ac.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.58ba97ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b43348f140784b09f76",
+    "url": "/static/js/main.7c961e81.chunk.js"
+  },
+  {
+    "revision": "ae0d5c4ffa5238c08f8f",
+    "url": "/static/js/runtime-main.9e426897.js"
+  },
+  {
+    "revision": "9fd390b3d180084bbca0e24e5836d12f",
+    "url": "/static/media/appStore.9fd390b3.png"
+  },
+  {
+    "revision": "43995255ba5b7d537dd6b5cf6bcf4fb8",
+    "url": "/static/media/asset1.43995255.png"
+  },
+  {
+    "revision": "b5380011fe168dafc1cc17f2e312e8e2",
+    "url": "/static/media/avatar.b5380011.png"
+  },
+  {
+    "revision": "6b48fec15c67dd5fa95eac35df2873b3",
+    "url": "/static/media/content1.6b48fec1.png"
+  },
+  {
+    "revision": "fb4eecc5992df35ef7ab97aaa0b7d815",
+    "url": "/static/media/content2.fb4eecc5.png"
+  },
+  {
+    "revision": "5e1a5cd81a8fe62fdd37dcefc07ffb5c",
+    "url": "/static/media/googlePlay.5e1a5cd8.png"
+  },
+  {
+    "revision": "1a934495ed8352f45c4513386a0bcd31",
+    "url": "/static/media/khata.1a934495.png"
+  },
+  {
+    "revision": "d94c749a73576d3274d030a7a20ecc69",
+    "url": "/static/media/logo.d94c749a.png"
+  },
+  {
+    "revision": "8e01a5c90801953b5f27df21ffbeb48a",
+    "url": "/static/media/quotes.8e01a5c9.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "6c415c274217ea8f8e8d2377538a1eb9",
+    "url": "/static/media/tick.6c415c27.png"
+  }
+]);
